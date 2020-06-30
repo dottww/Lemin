cb8953f160ffe79bf95bb064f6bb234bac0a830c
Md4 <matthieudavid404@gmail.com>
2020-06-30 11:24:57 +0200

    comments for all the functions within the file tool_links.c

--- src/tools_links.c
+++ src/tools_links.c
@@ -3,14 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   tools_links.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: weilin <weilin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mdavid <mdavid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 18:02:10 by weilin            #+#    #+#             */
-/*   Updated: 2020/06/27 00:29:54 by weilin           ###   ########.fr       */
+/*   Updated: 2020/06/30 11:24:34 by mdavid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
+
+/*
+** ___Description___:
+**	Function explore the list of link of the source room to find the link
+**	going to the dest room.
+** ___Return___:
+**	link: the link from the src room to dest room
+**	NULL: if no link between src room and dest room is found
+*/
 
 static t_list	*get_link(t_list *src, t_list *dest)
 {
@@ -28,6 +37,20 @@ static t_list	*get_link(t_list *src, t_list *dest)
 	return (NULL);
 }
 
+/*
+** ___Description___:
+**	Depending on value of is_previous (true if the previous room is src,
+**	false otherwise), the usage of the link between src and dest is set
+**	to -1|1 if usage was 0.
+**	If initially usage is -1|1, the usage is reset to 0 (occurs when a
+**	deviation took place via this link).
+** ___Remarks___:
+**	The usage allows to know which link was used to go from start to end.
+**	As mentionned, when a link was already used for a path and a deviation
+**	via this link is allowed, the usage is reset to 0. It is a trick the
+**	process of reconstruction of the augmented path (the different paths).
+*/
+
 static void		set_link_usage(t_list *src, t_list *dest, bool is_previous)
 {
 	t_list	*link;
@@ -43,12 +66,12 @@ static void		set_link_usage(t_list *src, t_list *dest, bool is_previous)
 }
 
 /*
-** Description:
+** ___Description___:
 **	After a successful BFS, route (queue of rooms) is free/destroyed here,
 **	plus the function update the usage of the links of the rooms constituting
 **	the path:
-**		usage of the link is set to -1 in the direction "start -> end",
-**		usage of the link is set to -1 in the direction "end -> start"
+**		- usage of the link is set to -1 in the direction "start -> end",
+**		- usage of the link is set to -1 in the direction "end -> start".
 */
 
 void			set_links_usage(t_list *end, t_list **route)
