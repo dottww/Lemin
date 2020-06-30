eea5d761f7b82e035a218fc3ab0a0a6aeae9925e
Md4 <matthieudavid404@gmail.com>
2020-06-30 11:08:42 +0200

    comments of all the functions in get_path.c

--- src/get_path.c
+++ src/get_path.c
@@ -12,30 +12,62 @@
 
 #include "lem_in.h"
 
+/*
+** ___Description___:
+**	Initialization of a path, all inner variable are set, and this new path
+**	is append in the list pth (the augmented path).
+**	Notice that only the 1st link (the 1st room after start) is allocated,
+**	each paths will be constructed in the function finish_path.
+** ___Remarks___:
+**	The id of paths change every times the function init_path is called,
+**	thus the id of the same path will change every time the augmented path
+**	is update.
+** ___Return___:
+**	1: if the initialization is successfully performed.
+**	0: if memory allocation issue
+*/
+
 static int		init_path_sub(t_list **pth, t_list *room)
 {
 	t_list		*new_path;
 	t_path		path;
 	static int	path_id = 0;
 
-	path.len = 1; // initialization of the length of the path based on the 1st room received (*room)
-	path.id = ++path_id; // definition ofthe path id, here static, thus last value keep in mem and increased at each round
-	path.pending = 0; // number of ants which will be sent
-	path.room = NULL; //
-	path.complete = false; // init of flag saying path reach the end or not (not by default)
-	if (!(new_path = ft_lstnew(&path, sizeof(t_path)))) // allocation of the head link from which the path starts
+	path.len = 1;
+	path.id = ++path_id;
+	path.pending = 0;
+	path.room = NULL;
+	path.complete = false;
+	if (!(new_path = ft_lstnew(&path, sizeof(t_path))))
 		return (0);
-	((t_path *)new_path->content)->room = room; // adding the room being the first of the path (not start but the one after, because start obviously the 1st and is present every time)
-	ft_lstprepend(pth, new_path); // append of the head of the new path in the list of differents pth
+	((t_path *)new_path->content)->room = room;
+	ft_lstprepend(pth, new_path);
 	return (1);
 }
+
+/*
+** ___Description___:
+**	Function is called after reaching the room end in BFS process.
+**	The 1st action performed is the update of the usage of the links
+**	constituting the last path found by the bfs process (last path because
+**	the usage are update based on the inner variable previous of each room
+**	visited in reversed order from end).
+**	Next, the list of paths (called augmented path in the graph theory) is
+**	initialized first (init_path_sub) and constructed (finish_path).
+** ___Remarks___:
+**	Notice that as deviation can occured, it is here, during set_links_usage
+**	and finish_path that 2 paths crossing each other are reconstruted.
+** ___Return___:
+**	1: if the augmented paths is fully constructed.
+**	0: if memory allocation issue
+*/
 
 static int		init_path(t_antfarm *atf, t_list **pth, t_list **route)
 {
 	t_list		*link;
 
 	set_links_usage(atf->end, route);
-	link = ((t_room *)(atf->start->content))->links; //catching all the links of start
+	link = ((t_room *)(atf->start->content))->links;
 	while (link)
 	{
 		if (((t_link *)link->content)->usage == -1)
@@ -55,7 +87,7 @@ static int		init_path(t_antfarm *atf, t_list **pth, t_list **route)
 **	*atf: pointer on the struct variable t_antfarm, containing full
 **		  description of the antfarm.
 **	**pth: adress of pointer on t_list struct variable, being list
-**			 of the differents pth and descriptive variable within
+**		   of the differents pth and descriptive variable within.
 ** ___Remarks___:
 **	As pth is a list, the t_path inner variable is at content:
 **	pth: [cnt:path1 & next->]--[cnt:path2 & next->]--{...}--[NULL]
@@ -64,8 +96,10 @@ static int		init_path(t_antfarm *atf, t_list **pth, t_list **route)
 **	and calculate the number of ants per pth and evaluate the performance
 **	(in how many rounds all the ants will reach the END)
 ** ___Return___:
-**	TRUE  : 
-**	FALSE : if rounds != LONG_MAX(initialization value) || ret = 0
+**	TRUE  : if a solution is found i.e. there is/are path(s) connecting start
+**			to end.
+**	FALSE : if rounds != LONG_MAX(initialization value) || ret = 0 or memory
+**			allocation issue.
 */
 
 static bool		find_path(t_antfarm *atf, t_list **pth)
@@ -75,14 +109,14 @@ static bool		find_path(t_antfarm *atf, t_list **pth)
 	t_list			*previous_pth;
 
 	route = NULL;
-	while (bfs_route(atf->start, atf->end, &route)) //list out all possible routes and complying to rules
+	while (bfs_route(atf->start, atf->end, &route))
 	{
 		previous_pth = *pth;
 		*pth = NULL;
-		if (!init_path(atf, pth, &route)) //unique id: path_id created on the fly
+		if (!init_path(atf, pth, &route))
 			return (false);
 		ret = solution_rounds(atf, *pth, atf->ant_qty);
-		if (ret >= atf->rounds || ret == 0) // control sequence, if ret is bigger to LONG_MAX (stocked in atf->rounds)
+		if (ret >= atf->rounds || ret == 0)
 		{
 			ft_lstdel(pth, ft_delcontent);
 			*pth = previous_pth;
@@ -97,12 +131,17 @@ static bool		find_path(t_antfarm *atf, t_list **pth)
 
 /*
 ** ___Description___:
-**	Check if the room start has the room end as a neighbor.
-**	If so, only one path is constructed.
-**	Otherwise, function 'find_path()' is called to find multiple pth
+**	Function looks for path(s) connecting the rooms start and end.
+**	Two cases are treated:
+**	  - if the room start and end are directly connected,
+**	  - if they are not.
+**	In the 1st case, only one path is constructed, in the latter case
+**	the algo of paths finding based on BFS is used.
 ** ___Return___:
-**	TRUE  : if no mem allocation issue in init_unique_path or .....
-**	FLASE : if mem allocation in init_unique_path or ......
+**	TRUE  : if no mem allocation issue or if there is at least one
+**			path found during BFS.
+**	FLASE : if mem allocation in any functions encounter in init_unique_path
+**			or in find_path.
 */
 
 bool			get_path(t_antfarm *atf, t_list *start, t_list *end
